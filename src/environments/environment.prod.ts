1b6c181f06599722225b7d66f6b6430e01452519
periyasamy1986 <periyasamyp.mca@gmail.com>
2024-12-03 22:00:49 +0530

    TAT Issue Fix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:8090/cheque',
+  apiUrl: 'http://10.10.46.203:9001/cheque',
   /*apiUrl: 'http://localhost:8090/cheque',*/
   featureFlag: true,
   logging: {
